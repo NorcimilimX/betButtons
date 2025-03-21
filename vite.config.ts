6938adc2b0b82bf9fe4c887a21a90f1fde929bec
norcimili.x <milimicronmilimitop@gmail.com>
2025-03-21 23:23:26 +0200

    provide base option to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: '/betButtons/',
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src') // Додаємо підтримку `@`
-        }
-    }
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
 })
